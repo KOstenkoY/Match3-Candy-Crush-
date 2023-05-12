52730df1ea961ef4016429adb729626a99a55eef
KOstenkoY <zhenya.kostenko.2003@gmail.com>
2023-05-12 12:32:54 +0300

    Integrate AppsFlyer plugin.

---
+++ Assets/Scripts/ConverionDataSuccess.cs
@@ -0,0 +1,17 @@
+using System;
+using AppsFlyerSDK;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConverionDataSuccess : MonoBehaviour
+{
+    [SerializeField] private AppsFlyerObjectScript _appsFlyerObject;
+    [SerializeField] private Text _conversionDataSeccessText;
+    string conversionData;
+
+    private void Start()
+    {
+        _appsFlyerObject.onConversionDataSuccess(conversionData);
+        _conversionDataSeccessText.text = conversionData;
+    }
+}
